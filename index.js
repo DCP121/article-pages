02ed2e5d0f7deff667ed6fce2ca8f0a3426f2e03
dharmik.j <dharmikjtridhyatech@gmail.com>
2023-10-23 16:40:10 +0530

    password toggle added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     loadCSS(
       "https://cdn.jsdelivr.net/gh/DCP121/article-pages@b557d7200627dfa0c23b270638ca94ac5e9aaa4e/index.css"
     );
-    loadCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
+    loadCSS("https://cdn.jsdelivr.net/gh/DCP121/article-pages@d9f38e1144a6a400f61641443cdd3df9e5dcba0b/index.css")
     //loadCSS("./index.css");
 
     // Load JavaScript libraries
@@ -46,9 +46,9 @@ document.addEventListener("DOMContentLoaded", function () {
                   // Create a script element
                   const script = document.createElement("script");
                   // var API_URL = "https://41dd-137-184-19-129.ngrok-free.app/api/v1"
-                  var API_URL = "https://505f-137-184-19-129.ngrok-free.app/api/v1"
+                  var API_URL = "https://60ad-137-184-19-129.ngrok-free.app/api/v1"
 
-                  var FILE_URL = "https://505f-137-184-19-129.ngrok-free.app"
+                  var FILE_URL = "https://60ad-137-184-19-129.ngrok-free.app"
 
                   // Set the source and other attributes for the script
                   script.src =
