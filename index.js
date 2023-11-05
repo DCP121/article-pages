54478a1405d18d57eaa8ea3844e079b9291b6981
HarshalGhadiya <harshalgtridhyatech@gmail.com>
2023-11-05 11:09:31 +0530

    add login console

--- index.js
+++ index.js
@@ -2835,7 +2835,11 @@ document.addEventListener("DOMContentLoaded", function () {
                             "userData",
                             JSON.stringify(response?.data?.data?.user)
                           );
-                          localStorage.setItem('captcha', iscaptchaVerified)
+                         
+                          var captchaValue = sessionStorage.getItem('captcha')
+                          console.log(captchaValue , iscaptchaVerified ,2840)
+
+                          localStorage.setItem('captcha', captchaValue)
                           commentlistapi(true);
                           $Login.css({ display: "none" });
                           $Register.css({ display: "none" });
