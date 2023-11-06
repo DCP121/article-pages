9fb286b3d1e3016e0c45faa2b3227808b14f3769
HarshalGhadiya <harshalgtridhyatech@gmail.com>
2023-11-06 18:16:24 +0530

    .

--- index.js
+++ index.js
@@ -2228,10 +2228,10 @@ document.addEventListener("DOMContentLoaded", function () {
                                       firstcheckforV3 = true
                                       // isVersion3 = false
 
-                                      var Token = localStorage.getItem('token')
-                                      // console.log(Token, 'Token', !!Token, typeof Token, Token != null, 996, Token === true, Token == "true")
+                                      var replyToken = localStorage.getItem('token')
+                                      console.log(replyToken, 'replyToken', !!replyToken, typeof replyToken, replyToken != null, 996, replyToken === true, replyToken == "true")
 
-                                      Token === true || Token == "true" ? localStorage.setItem('captcha', iscaptchaVerified) : sessionStorage.setItem('captcha', iscaptchaVerified)
+                                      replyToken === true || replyToken == "true" ? localStorage.setItem('captcha', iscaptchaVerified) : sessionStorage.setItem('captcha', iscaptchaVerified)
 
                                       if (userData?.emailVerified === true) {
                                         submitReplyComment();
